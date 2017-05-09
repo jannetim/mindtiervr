3b6be8acdad1de585ba7fa46481b169e7b7dc25e
Antti Ruonala <antti.ruonala@aalto.fi>
2017-05-09 15:44:36 +0300

    pulsating halos

---
+++ Assets/AuraScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraScaler : MonoBehaviour {
+	float scalefactor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scalefactor = (0.40f + 0.15f * (Mathf.Sin (Time.time * 0.8f)*0.5f + 1f));
+		this.transform.localScale = new Vector3 ( scalefactor, scalefactor, scalefactor) ;
+
+		
+	}
+}
