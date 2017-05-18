f052fcad9ed18c6be6e946f2a169760366475185
Janne Timonen <petrutsi@gmail.com>
2017-05-18 20:28:53 +0300

    first version of synced breathe bars

--- Assets/Scripts/BreathLayerer.cs
+++ Assets/Scripts/BreathLayerer.cs
@@ -7,28 +7,37 @@ public class BreathLayerer : MonoBehaviour {
     public GameObject[] planes;
     private Color planeColor;
     private GameObject player;
-    private GameObject otherPlayerLayers;
+    private GameObject otherPlane, otherPlane2;
+    private GameObject plane, plane2;
     [Range(0, 1)]
     public float PlaneTransparency = 0.2f;
     private float origAlpha;
     private bool isFadingIn;
     private bool isFadingOut;
+    private float ownH, ownS, ownV;
+    private float otherH, otherS, otherV;
     // Use this for initialization
     void Start () {
         System.Array.Reverse(planes);
         if (gameObject.name == "Player1_BridgeLayers")
         {
             player = GameObject.Find("Player1_Manager");
-            otherPlayerLayers = GameObject.Find("Player2_BridgeLayers/planes");
+            plane = GameObject.Find("Player1_BridgeLayers/Plane5");
+            plane2 = GameObject.Find("Player1_BridgeLayers/Plane4");
+            otherPlane = GameObject.Find("Player2_BridgeLayers/Plane5");
+            otherPlane2 = GameObject.Find("Player2_BridgeLayers/Plane4");
         } else if (gameObject.name == "Player2_BridgeLayers")
         {
             player = GameObject.Find("Player2_Manager");
-            otherPlayerLayers = GameObject.Find("Player1_BridgeLayers/planes");
+            plane = GameObject.Find("Player2_BridgeLayers/Plane5");
+            plane2 = GameObject.Find("Player2_BridgeLayers/Plane4");
+            otherPlane = GameObject.Find("Player1_BridgeLayers/Plane5");
+            otherPlane2 = GameObject.Find("Player1_BridgeLayers/Plane4");
         }
-
         origAlpha = PlaneTransparency;
-       // InvokeRepeating("InitBreatheBar", 2.0f, 5.0f);
-	}
+
+        // InvokeRepeating("InitBreatheBar", 2.0f, 5.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -51,21 +60,46 @@ public class BreathLayerer : MonoBehaviour {
         }
         yield return StartCoroutine("FadeIn");
         isFadingIn = false;
+
+        if (otherPlane.activeInHierarchy && otherPlane.GetComponent<Renderer>().material.color.a > 0.1f)
+        {
+            StartCoroutine("SyncGlow");
+        }
         yield return StartCoroutine("FadeOut");
         isFadingOut = false;
+    }
+
+    IEnumerator FadeIn()
+    {
+        isFadingIn = true;
+        foreach (GameObject o in planes)
+        {
+            planeColor.a = 0;
+            o.GetComponent<Renderer>().material.SetColor("_Color", planeColor);
+            o.SetActive(false);
+        }
+        System.Array.Reverse(planes);
+        foreach (GameObject o in planes)
+        {
+            o.SetActive(true);
+            Color color = o.GetComponent<Renderer>().material.color;
+            for (float f = 0; f <= origAlpha; f += 0.05f)
+            {
+                color.a = f;
+                o.GetComponent<Renderer>().material.SetColor("_Color", color);
+                yield return null;
+            }
+        }
     }
 
     IEnumerator FadeOut()
     {
         isFadingOut = true;
         yield return new WaitForSeconds(1.5f);
-        //print("out");
         System.Array.Reverse(planes);
         foreach (GameObject o in planes)
         { 
             Color color = o.GetComponent<Renderer>().material.color;
-            //Debug.Log(color.a);
-            //float origAlpha = color.a;
             for (float f = origAlpha; f >= 0; f -= 0.005f)
             {
                 color.a = f;
@@ -76,32 +110,62 @@ public class BreathLayerer : MonoBehaviour {
         }
     }
 
-    IEnumerator FadeIn()
+    IEnumerator SyncGlow()
     {
-        isFadingIn = true;
-        //print("in");
-        foreach (GameObject o in planes)
+        yield return StartCoroutine("SyncGlowIn");
+        yield return StartCoroutine("SyncGlowOut");
+    }
+
+    IEnumerator SyncGlowIn()
+    {
+        Color colorOwn = plane.GetComponent<Renderer>().material.color;
+        Color colorOther = otherPlane.GetComponent<Renderer>().material.color;
+
+        /*float alpha = colorOwn.a;
+        for (float f = alpha; f < alpha+0.4f; f += 0.001f)
         {
-            planeColor.a = 0;
-            o.GetComponent<Renderer>().material.SetColor("_Color", planeColor);
-            o.SetActive(false);
-            //print(o.GetComponent<Renderer>().material.color);
+            colorOwn.a = f;
+            plane.GetComponent<Renderer>().material.SetColor("_Color", colorOwn);
         }
-        System.Array.Reverse(planes);
-        foreach (GameObject o in planes)
+        alpha = colorOther.a;
+        for (float f = alpha; f < alpha + 0.4f; f += 0.001f)
         {
-            o.SetActive(true);
-            //Debug.Log("enter fadeout function");
-            Color color = o.GetComponent<Renderer>().material.color;
-            //Debug.Log(color.a);
-            //float origAlpha = color.a;
-            for (float f = 0; f <= origAlpha; f += 0.05f)
-            {
-                color.a = f;
-                o.GetComponent<Renderer>().material.SetColor("_Color", color);
-                yield return null;
-            }
+            colorOther.a = f;
+            otherPlane.GetComponent<Renderer>().material.SetColor("_Color", colorOther);
+        }*/
+        Color.RGBToHSV(colorOwn, out ownH, out ownS, out ownV);
+        Color.RGBToHSV(colorOther, out otherH, out otherS, out otherV);
+        for (float f = 0; f < 1.0f; f += 0.01f ) {
+            plane.GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(ownH, ownS, ownV + f));
+            otherPlane.GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(otherH, otherS, otherV + f));
+            yield return null;
         }
-        
+    }
+
+    IEnumerator SyncGlowOut()
+    {
+        Color colorOwn = plane.GetComponent<Renderer>().material.color;
+        Color colorOther = otherPlane.GetComponent<Renderer>().material.color;
+
+        /*float alpha = colorOwn.a;
+        for (float f = colorOwn.a; f >= origAlpha; f -= 0.001f)
+        {
+            colorOwn.a = f;
+            plane.GetComponent<Renderer>().material.SetColor("_Color", colorOwn);
+        }
+        for (float f = colorOther.a; f >= origAlpha; f -= 0.001f)
+        {
+            colorOther.a = f;
+            otherPlane.GetComponent<Renderer>().material.SetColor("_Color", colorOther);
+        }*/
+
+        Color.RGBToHSV(colorOwn, out ownH, out ownS, out ownV);
+        Color.RGBToHSV(colorOther, out otherH, out otherS, out otherV);
+        for (float f = 1.0f; f >= 0; f -= 0.01f)
+        {
+            plane.GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(ownH, ownS, ownV - f));
+            otherPlane.GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(otherH, otherS, otherV - f));
+            yield return null;
+        }
     }
 }
