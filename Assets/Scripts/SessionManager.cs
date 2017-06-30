7afdc29f37bc087d3107e78126e63c323a2c9eef
Janne Timonen <petrutsi@gmail.com>
2017-06-30 19:48:24 +0300

    debugging

--- Assets/Scripts/SessionManager.cs
+++ Assets/Scripts/SessionManager.cs
@@ -245,6 +245,7 @@ public class SessionManager : MonoBehaviour
                     } else
                     {
                         NetworkManager.Shutdown();
+                        yield return new WaitForSeconds(0.5f);
                     }
                 }
             }
