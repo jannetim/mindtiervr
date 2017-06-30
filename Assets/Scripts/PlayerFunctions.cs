c0ec82c56cfb7e15f78915d2542ed840428e4993
Janne Timonen <petrutsi@gmail.com>
2017-06-30 21:16:15 +0300

    deleted obsolete stuff from playerfunctions

--- Assets/Scripts/PlayerFunctions.cs
+++ Assets/Scripts/PlayerFunctions.cs
@@ -11,57 +11,16 @@ public class PlayerFunctions : NetworkBehaviour {
 
     // Use this for initialization
     void Start () {
-        /* if (GameObject.Find("Session Manager").GetComponent<SessionManager>().SingleUserSession)
-         {
-             if (gameObject.name != "PlayerSim")
-             {
-                 Cam.enabled = true;
-                 AudioListener.enabled = true;
-             }
-
-         }
-         else */
         if (!isLocalPlayer)
         {
             return;
         }
         Cam.enabled = true;
         AudioListener.enabled = true;
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!SceneManager.GetActiveScene().name.Equals("ForestShrine16_network"))
-        {
-            transform.gameObject.GetComponent<PlayerFAScript>().enabled = false;
-        }
-        /*Renderer renderer = bridge.GetComponent<Renderer>();
-        Material material = renderer.materials[1];
-        float faValue = Mathf.Abs(Mathf.Sin(Time.time * 0.2f)) + 0.5f;
 
-        material.SetColor("_EmissionColor", new Color(faValue, faValue, faValue));
-        */
-        /*
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-
-    */
-
-        /*if (Input.GetKeyDown("r"))
-        {
-            Debug.Log("pressed r");
-            GameObject.Find("Network Manager").GetComponent<NetworkManager>().StopHost();
-            GameObject.Find("Network Manager").GetComponent<NetworkManager>().StopServer();
-            //GameObject.Find("Network Manager").GetComponent<NetworkManager>().ServerChangeScene("LaunchManager");
-            SceneManager.LoadScene(3);
-        }*/
     }
 }
