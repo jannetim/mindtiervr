e6d79f252d70741e6cab342ed5ae325f16db0eb5
antti ruonala <antti.ruonala@gmail.com>
2017-07-05 18:17:46 +0300

    FA sync treshold set to 0.2 instead of 0.1

--- Assets/Scripts/PlayerFAScript.cs
+++ Assets/Scripts/PlayerFAScript.cs
@@ -161,7 +161,7 @@ public class PlayerFAScript : NetworkBehaviour
 				fasync = Mathf.Abs (PlayerFA_Display - OtherFA);
 
 				// Lower and flickering emission saturation according to FA-level when FA-levels in sync
-				if (fasync < 0.1)
+				if (fasync < 0.2f)
                 {
                     //Debug.Log("IN SYNC " + fasync);
                     if (glowS > 0.75f && !flickerS) {
